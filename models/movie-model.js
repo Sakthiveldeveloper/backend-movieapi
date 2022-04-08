8c053068650623d9223366533ad3d68646c02258
Sakthiveldeveloper <sakthivel24498@gmail.com>
2022-04-08 19:51:49 +0530

    add schemas

--- models/movie-model.js
+++ models/movie-model.js
@@ -3,37 +3,42 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true},
-  creator: { type: String, required: true }
+  moviename: { type: String, required: true },
+  description: { type: String, required: true },
+  creator: { type: String, required: true },
+  rating: { type: Number, required: true },
+  cast: { type: Array, required: true },
+  gender: { type: String, required: true },
+  releasetime: { type: Date, required: true }
+
 });
 
 module.exports = mongoose.model('Movie', userSchema);
 // CREATE MOVIE 
 const createMovie = async (req, res) => {
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) 
-       return res.status(400).json({ errors: errors.array() });
+  const errors = validationResult(req);
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() });
 
-    const { title, description, creator } = req.body;
-    let  existingUser = await User.findOne({ _id: creator });
-    if (!existingUser)
-       return res.status(400).json({error : 'invalid user'});
+  const { title, description, creator } = req.body;
+  let existingUser = await User.findOne({ _id: creator });
+  if (!existingUser)
+    return res.status(400).json({ error: 'invalid user' });
 
-    const movie = new Movie({
-        title,
-        description,
-        creator
-    });
+  const movie = new Movie({
+    title,
+    description,
+    creator
+  });
 
-    try {
-        await movie.save();
-        res.status(200).json({
-        message: "movie created successfully",
-        movie: movie
-      })
-    } catch (error) {
-         res.status(500).json(error.message);
-    }
+  try {
+    await movie.save();
+    res.status(200).json({
+      message: "movie created successfully",
+      movie: movie
+    })
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
